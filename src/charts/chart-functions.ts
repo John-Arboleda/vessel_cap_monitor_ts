2a86831856a5e676146c85fe5bf76e347d047256
John-Arboleda <johnjack0126@gmail.com>
2025-04-18 22:44:57 -0500

    Fix filter of 4D table chart

--- src/charts/chart-functions.ts
+++ src/charts/chart-functions.ts
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
 
-const baseYear: number = 1996;
+const baseYear: number = 1995;
 const T: number = 55;
 const I: number = 5;
 // const V: number = 2;
@@ -23,9 +23,15 @@ function sumPeriod(
     // Iterate over the specified 'vesselKeys'
     for (let v = 0; v < vesselKeys.length; v++) {
       let techKey = vesselKeys[v];
+
+      try{
+        result += dataProp[techKey][sizeKey][t];
+      } catch (error) {
+        console.log(dataProp, techKey, sizeKey, t);
+      }
       // console.log(dataProp, techKey, sizeKey, t);
       // Add the value at the specified indices to the result
-      result += dataProp[techKey][sizeKey][t];
+      // result += dataProp[techKey][sizeKey][t];
     }
   }
 
@@ -41,6 +47,47 @@ function sumDataObj(
   const dataArr: number[] = new Array(T).fill(0);
 
   return dataArr.map((_t, j) => sumPeriod(dataProp, j, vesselKeys, sizeKeys));
+}
+
+function sumPeriod2(
+  dataProp: number[][][][],
+  t: number,
+  regionKeys: number[],
+  vesselKeys: number[] = [0, 1, 2, 3, 4],
+  sizeKeys: number[] = [0, 1, 2, 3, 4]
+): number {
+  let result: number = 0;
+
+  // Iterate over the specified 'regionKeys'
+  for (let r = 0; r < regionKeys.length; r++) {
+    let regionKey = regionKeys[r];
+    // Iterate over the specified 'sizeKeys'
+    for (let i = 0; i < sizeKeys.length; i++) {
+      let sizeKey = sizeKeys[i];
+
+      // Iterate over the specified 'vesselKeys'
+      for (let v = 0; v < vesselKeys.length; v++) {
+        let vesselkey = vesselKeys[v];
+
+        // Add the value at the specified indices to the result
+        result += dataProp[regionKey][vesselkey][sizeKey][t];
+        
+      }
+    }
+  }
+  return result;
+}
+
+function sumDataObj2(
+  dataProp: number[][][][],
+  regionKeys: number[],
+  vesselKeys: number[] = [0, 1, 2, 3, 4],
+  sizeKeys: number[] = [0, 1, 2, 3, 4]
+): number[] {
+
+  const dataArr: number[] = new Array(T).fill(0);
+
+  return dataArr.map((_t, j) => sumPeriod2(dataProp, j, regionKeys, vesselKeys, sizeKeys));
 }
 
 function createDataFleetLines(
@@ -93,12 +140,10 @@ function sumPeriodRegions(
   sizeKeys: number[] = [0, 1, 2, 3, 4],
 ){
 
-  const regionsVesselArr = regionKeys.map(route => {
-    return sumRegion(matrix4D[route], vesselKeys, sizeKeys
-    )
-  })
-
-  const sumN:  number[][] = vesselKeys.map(v => sumDataObj(regionsVesselArr, regionKeys, [v]))
+  const sumN:  number[][] = vesselKeys.map(v => {
+    // return vesselKeys.length > 1 ? sumDataObj(regionsVesselArr, regionKeys, [v]) :
+      return sumDataObj2(matrix4D, regionKeys, [v], sizeKeys);
+      })
 
   const transSumN: number[][] = d3.transpose(sumN);
 
@@ -110,26 +155,9 @@ function sumPeriodRegions(
     dataArr.push(dataPeriod);
   }
 
+  console.log(dataArr);
+
   return dataArr;
-}
-
-function sumRegion(
-  dataProp: number[][][],
-  vesselKeys: number[] = [0, 1, 2, 3, 4],
-  sizeKeys: number[] = [0, 1, 2, 3, 4]
-): number[][] {
-  
-  const sumN: number[][] = [];
-
-  vesselKeys.forEach((tech: number) => {
-    sumN.push(sumDataObj(dataProp, [tech], sizeKeys));
-  })
-
-  // const transSumN = d3.transpose(sumN);
-  
-  // return transSumN;
-
-  return sumN;
 }
 
 function createGapBySize(
